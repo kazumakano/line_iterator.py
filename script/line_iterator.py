c09461fc3867daa3301f93ca3f0c509502836e61
kazumakano <kazumakano@outlook.jp>
2021-10-24 21:20:23 +0900

    minor modify

--- script/line_iterator.py
+++ script/line_iterator.py
@@ -5,9 +5,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 from skimage.draw import line
 
-BGR: int = 0
-RGB: int = 1
-GRAY: int = 2
+BGR_COLOR_SPACE: int = 0
+RGB_COLOR_SPACE: int = 1
+GRAY_COLOR_SPACE: int = 2
 VMIN = np.uint8(0)
 VMAX = np.uint8(255)
 
@@ -20,19 +20,19 @@ class LineIterator(np.ndarray):
 
         return np.array(img[offsets[0], offsets[1]]).view(type=cls)
 
-    def show(self, color_space: int = BGR) -> None:
-        if color_space == BGR:
+    def show(self, color_space: int = BGR_COLOR_SPACE) -> None:
+        if color_space == BGR_COLOR_SPACE:
             axes: np.ndarray = plt.subplots(nrows=4, sharex=True)[1]
             line_img: np.ndarray = cv2.cvtColor(self[np.newaxis], cv2.COLOR_BGR2RGB)    # convert from BGR to RGB
             _show_rgb_bar(axes[0], line_img)
             _plot_rgb_value(axes[1:], line_img[0])
 
-        elif color_space == RGB:
+        elif color_space == RGB_COLOR_SPACE:
             axes: np.ndarray = plt.subplots(nrows=4, sharex=True)[1]
             _show_rgb_bar(axes[0], self[np.newaxis])
             _plot_rgb_value(axes[1:], self)
         
-        elif color_space == GRAY:
+        elif color_space == GRAY_COLOR_SPACE:
             axes: np.ndarray = plt.subplots(nrows=2, sharex=True)[1]
             _show_gray_bar(axes[0], self[np.newaxis])
             _plot_gray_value(axes[1], self)
@@ -48,11 +48,11 @@ def _show_gray_bar(ax: plt.Axes, line_img: np.ndarray) -> None:
     ax.imshow(line_img, cmap="gray", aspect="auto", vmin=VMIN, vmax=VMAX)
 
 def _plot_rgb_value(axes: np.ndarray, line_iterator: LineIterator) -> None:
-    rgb = ("red", "green", "blue")
+    colors = ("red", "green", "blue")
     for i, a in enumerate(reversed(axes)):
         a.set_ylim(VMIN, VMAX)
         a.set_yticks((VMIN, VMAX))
-        a.plot(line_iterator[:, i], color=rgb[i])
+        a.plot(line_iterator[:, i], color=colors[i])
 
 def _plot_gray_value(ax: plt.Axes, line_iterator: LineIterator) -> None:
     ax.set_ylim(VMIN, VMAX)
