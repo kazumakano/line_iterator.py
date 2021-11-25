b6efb85a9b53873d1fe3932c6143d0cbc1a4350c
kazumakano <kazumakano@outlook.jp>
2021-11-25 14:34:15 +0900

    minor modify

--- script/sample.py
+++ script/sample.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
-import line_iterator as li
+from line_iterator import LineIterator
 
 IMG_FILE = "path to your image file"
 
 def sample():
     img: np.ndarray = cv2.imread(IMG_FILE)
 
-    line_iterator = li.LineIterator(img, (0, 0), (img.shape[0] - 1, img.shape[1] - 1))
+    line_iterator = LineIterator(img, (0, 0), (img.shape[0] - 1, img.shape[1] - 1))
 
     print(f"line length is {len(line_iterator)}")
 
