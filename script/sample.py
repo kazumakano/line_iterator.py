a411e97759e40989237780622abed44bd08a8e90
kazumakano <kazumakano@outlook.jp>
2022-01-07 18:22:05 +0900

    minor update

--- script/sample.py
+++ script/sample.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("img_file", help="specify path to your image file")
+    parser.add_argument("img_file", help="specify path to your image file", metavar="PATH_TO_IMG_FILE")
     img_file: str = parser.parse_args().img_file
 
     sample(img_file)
