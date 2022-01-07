73902acdda3dff5fa6008cc7c4d615f14001da72
kazumakano <kazumakano@outlook.jp>
2022-01-07 17:44:57 +0900

    minor modify

--- script/line_iterator.py
+++ script/line_iterator.py
@@ -8,8 +8,6 @@ from skimage.draw import line
 BGR_COLOR_SPACE = 0
 RGB_COLOR_SPACE = 1
 GRAY_COLOR_SPACE = 2
-VMIN = np.uint8(0)
-VMAX = np.uint8(255)
 
 def _show_rgb_bar(ax: plt.Axes, line_img: np.ndarray) -> None:
     ax.set_yticks(())
@@ -17,28 +15,29 @@ def _show_rgb_bar(ax: plt.Axes, line_img: np.ndarray) -> None:
 
 def _show_gray_bar(ax: plt.Axes, line_img: np.ndarray) -> None:
     ax.set_yticks(())
-    ax.imshow(line_img, cmap="gray", aspect="auto", vmin=VMIN, vmax=VMAX)
+    ax.imshow(line_img, cmap="gray", aspect="auto", vmin=0, vmax=255)
 
 def _plot_rgb_values(axes: np.ndarray, line_iterator: LineIterator) -> None:
     colors = ("red", "green", "blue")
     for i, a in enumerate(reversed(axes)):
-        a.set_ylim(VMIN, VMAX)
-        a.set_yticks((VMIN, VMAX))
+        a: plt.Axes
+        a.set_ylim(0, 255)
+        a.set_yticks((0, 255))
         a.plot(line_iterator[:, i], color=colors[i])
 
 def _plot_gray_values(ax: plt.Axes, line_iterator: LineIterator) -> None:
-    ax.set_ylim(VMIN, VMAX)
-    ax.set_yticks((VMIN, VMAX))
+    ax.set_ylim(0, 255)
+    ax.set_yticks((0, 255))
     ax.plot(line_iterator, color="black")
 
 class LineIterator(np.ndarray):
     def __new__(cls, img: np.ndarray, pt1: Any, pt2: Any) -> LineIterator:
         if type(img) != np.ndarray:
             raise Exception(f"type of image is expected to be 'numpy.ndarray' but {type(img)} was given")
-        if img.ndim not in (2, 3):
-            raise Exception(f"the number of dimensions of image is expected to be 2 or 3 but {img.ndim} was given")
         if img.dtype != np.uint8:
             raise Exception(f"data type of image is expected to be 'uint8' but {img.dtype} was given")
+        if img.ndim not in (2, 3):
+            raise Exception(f"the number of dimensions of image is expected to be 2 or 3 but {img.ndim} was given")
         if len(pt1) != 2 or len(pt2) != 2:
             raise Exception(f"length of points are expected to be 2 but {len(pt1)} and {len(pt2)} were given")
 
