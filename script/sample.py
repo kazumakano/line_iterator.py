ae378992c83b0bda1d4c067898b39d583292dc33
kazumakano <kazumakano@outlook.jp>
2022-01-07 18:12:36 +0900

    apply argument

--- script/sample.py
+++ script/sample.py
@@ -1,17 +1,25 @@
+from typing import Union
 import cv2
 import numpy as np
 from line_iterator import LineIterator
 
-IMG_FILE = "path to your image file"
 
-def sample():
-    img: np.ndarray = cv2.imread(IMG_FILE)
+def sample(file: str):
+    img: Union[np.ndarray, None] = cv2.imread(file)
+    if img is None:
+        raise Exception(f"{file} can't be loaded")
 
-    line_iterator = LineIterator(img, (0, 0), (img.shape[0] - 1, img.shape[1] - 1))
+    line_iterator = LineIterator(img, (0, 0), (img.shape[1] - 1, img.shape[0] - 1))
 
     print(f"line length is {len(line_iterator)}")
 
     line_iterator.show()
 
 if __name__ == "__main__":
-    sample()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("img_file", help="specify path to your image file")
+    img_file: str = parser.parse_args().img_file
+
+    sample(img_file)
